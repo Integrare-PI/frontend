fb79893d74f8cdb4f065a44904ce39930a2cd69b
BrunoRequena <brunorequena15@hotmail.com>
2022-04-08 13:17:48 -0300

    att feed

--- src/paginas/feed/Feed.tsx
+++ src/paginas/feed/Feed.tsx
@@ -1,65 +1,79 @@
-import React, { useEffect } from 'react';
-import { Typography, Box, Grid, Button } from '@material-ui/core';
-import './Feed.css';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import { Link, useHistory } from 'react-router-dom'
+import { busca } from '../../services/Services';
 import useLocalStorage from 'react-use-localstorage';
+import Postagem from '../../models/Postagem';
+import './Feed.css';
 import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { TokenState } from '../../store/tokens/tokensReducer';
 
-
 function Feed() {
-    let history = useHistory();
-    const token = useSelector<TokenState, TokenState["tokens"]>(
-        (state) => state.tokens
+  const [posts, setPosts] = useState<Postagem[]>([])
+
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
     );
+  let history = useHistory();
 
-    useEffect(() => {
-        if (token == "") {
-            toast.error('Você precisa estar logado!', {
-                position: "top-right",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: false,
-                draggable: false,
-                theme: "colored",
-                progress: undefined,
+  useEffect(() => {
+    if (token == "") {
+      toast.error('Você precisa estar logado!', {
+        position: "top-right",
+        autoClose:2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        theme: "colored",
+        progress: undefined,
 
-            })
-            history.push("/login")
+    })
+      history.push("/login")
 
-        }
-    }, [token])
-    return (
-        <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
-                <Grid alignItems="center" item xs={5} className='caixa1'>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Bem vinde!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>A Integrare ajuda você a se conectar e compartilhar com educadores especializados!</Typography>
-                    </Box>
-                    <Box display="flex" justifyContent="center">
-                        <Box>
-                            <Link to="cadastrousuario" className="text-decorator-none">
-                                <Button variant="outlined" className='botao'>Comece aqui</Button>
-                            </Link>
-                        </Box>
+    }
+  }, [token])
 
-                        <Box>
-                            <Link to="/contatos" className="text-decorator-none">
-                                <Button variant="outlined" className='botao'>Nos conheça melhor</Button>
-                            </Link>
-                        </Box>
+  async function getPost() {
+    await busca("/postagens", setPosts, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
 
-                    </Box>
-                </Grid>
-                <Grid item xs={7} className='imagemhome'></Grid>
+  useEffect(() => {
 
-            </Grid>
-        </>
-    );
+    getPost()
+
+  }, [posts.length])
+  return (
+    <>
+      {
+        posts.map(post => (
+          <Box m={2} >
+            <Card variant="outlined" className='cards'>
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Postagens
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {post.assunto}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {post.texto_descricao}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {post.tema?.descricao}
+                </Typography>
+              </CardContent>    
+            </Card>
+          </Box>
+        ))
+      }
+    </>)
 }
 
 export default Feed;
+//att
